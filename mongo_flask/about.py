00d0d9bc7a62ed1dbd24ee99da0cdfb6ada79a6b
juanmanuel96 <juanmapr913@gmail.com>
2021-05-20 00:28:06 -0400

    #2 Hotfix to update version number and include necessary files when installing package

--- mongo_flask/about.py
+++ mongo_flask/about.py
@@ -1,7 +1,7 @@
 __title__ = 'MongoFlask'
 __description__ = 'A Python Flask library for connecting a MongoDB instance to a Flask application'
 __url__ = 'https://github.com/juanmanuel96/mongo-flask'
-__version_info__ = ('0', '0', '2')
+__version_info__ = ('0', '1', '1')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Juan Vazquez'
 __py_version__ = 3
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
-from mongo_flask.about import __version__, __description__, __author__
+from mongo_flask.about import __version__, __description__, __author__, __url__
 
 
 setup(
     name='mongo_flask',
     version=__version__,
     description=__description__,
-    url=None,
+    url=__url__,
     author=__author__,
     author_email=None,
     license='BSD-2-Clause License',
-    packages=['mongo_flask'],
+    packages=['mongo_flask', 'mongo_flask.core', 'mongo_flask.errors'],
     install_requires=['flask', 'pymongo'],
     classifiers=[
         'Development Status :: 2 - Development',
